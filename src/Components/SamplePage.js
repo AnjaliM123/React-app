14bc11d0dcc9d4561191809ce3ac1e97887accf5
Anjali <anjalimarella3@gmail.com>
2021-09-28 19:11:15 +0530

    rename nature to nature images

--- src/Components/SamplePage.js
+++ src/Components/SamplePage.js
@@ -4,7 +4,7 @@ const SamplePage=()=> {
     return (
         <div class="main-container">
             
-            <h1 className="nature-heading">Nature</h1>
+            <h1 className="nature-heading">Nature images</h1>
             <div>
                 <img src="https://res.cloudinary.com/ddcycgbwg/image/upload/v1632745122/Screen-Shot-2020-04-21-at-8.39.04-AM-730x516_dzd4br.png" alt="nature pic" className="image"/>
                 <img src="https://res.cloudinary.com/ddcycgbwg/image/upload/v1632745122/Screen-Shot-2020-04-21-at-8.39.04-AM-730x516_dzd4br.png" alt="nature pic" className="image"/>
